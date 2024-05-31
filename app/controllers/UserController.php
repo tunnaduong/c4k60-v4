fe03b5cbbe0b77fa5c6c504da4a8c9ca79d52716
Duong Tung Anh <tunnaduong@gmail.com>
2024-05-31 19:49:15 +0700

    refactor: Update file paths, remove unused files, and add logout functionality, user avatar and greeting logic, menu page, and login functionality

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -10,6 +10,10 @@ class UserController extends BaseController
     {
         $user = new User();
         $profile = $user->userProfile($username);
+        // check if user is not found
+        if (!$profile) {
+            return $this->render("pages.errors.404");
+        }
         return $this->render("pages.user.profile", compact("profile"));
     }
 }
