d3aff747da48b83c51312ffa8db97ea47d0f13ca
Duong Tung Anh <tunnaduong@gmail.com>
2024-06-02 14:09:38 +0700

    refactor: Update sticky header style on screen layout

--- app/views/layouts/screen.blade.php
+++ app/views/layouts/screen.blade.php
@@ -11,7 +11,7 @@
     <div id="screen">
         <div id="root" style="background-color: @yield('background-color', 'white')">
             <div
-                style="display: flex;flex-direction:row;align-items:center;background: rgba(255, 255, 255, 0.75);backdrop-filter: blur(18px);margin: -15px -15px 10px;padding: 15px;border-bottom: 1px solid #e5e5e5;position:sticky;top:-15px;z-index: 99">
+                style="display: flex;flex-direction:row;align-items:center;background: rgba(255, 255, 255, 0.75);backdrop-filter: blur(18px);margin: -15px -15px 10px;padding: 15px;border-bottom: 1px solid #e5e5e5;position:sticky;top:-15px;z-index: 99;-webkit-backdrop-filter: blur(18px);">
                 <ion-icon name="arrow-back-outline" style="position: absolute;font-size: 24px;top: 18px"
                     onclick="history.back()"></ion-icon>
                 <span class="default-panel--title" style="flex: 1;text-align:center">@yield('title')</span>
