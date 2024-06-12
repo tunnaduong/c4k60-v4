573e92409335ec69edd8c6e7b7a82d8103ca8b19
Duong Tung Anh <tunnaduong@gmail.com>
2024-06-12 11:04:10 +0700

    Update notification screen

--- app/views/pages/notifications/index.blade.php
+++ app/views/pages/notifications/index.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'Thông báo lớp')
 
 @section('content')
-    <div>
-        <div style="background-color: #dbeafe">Tất cả</div>
-        <div>Chưa đọc</div>
+    <div style="display: flex;flex-direction:row;gap:10px">
+        <div style="background-color: #dbeafe;color:#5489f0;padding: 5px 10px;border-radius:25px;font-weight: 500"
+            class="text-zoom" goto>Tất cả
+        </div>
+        <div style="background-color: #f3f4f6;padding: 5px 10px;border-radius: 25px;font-weight: 500" class="text-zoom" goto>
+            Chưa
+            đọc</div>
+    </div>
+    <div style="display: flex;flex-direction:row;justify-content:space-between;margin-top: 15px;align-content:center">
+        <div class="text-zoom" style="font-size: 18px;font-weight:600">Từ ban cán sự lớp</div>
+        <div class="text-zoom" style="line-height: 30px;color:#5489f0" goto>Xem tất cả</div>
     </div>
 @endsection
 
