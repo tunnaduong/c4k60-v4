df67568f9f22e0bea4dcfd5a3b0286c47028e950
Duong Tung Anh <tunnaduong@gmail.com>
2024-06-29 15:15:20 +0700

    refactor: Update gallery page layout and fix image display issue

--- app/views/pages/gallery/album.blade.php
+++ app/views/pages/gallery/album.blade.php
@@ -4,11 +4,11 @@
 @section('background-color', '#ededed')
 @section('content')
     @if (isset($images))
-        <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px">
+        <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px;margin-right:-10px">
             @foreach ($images as $image)
-                <div class="w-[32.1%] h-auto ripple" data-bs-toggle="modal" data-bs-target="#imageViewer"
-                    data-img-src="{{ '/anhvavideo\/' . $image->path }}">
-                    <img style="height: 180px;width: 100%;object-fit:cover" src="{{ '/anhvavideo\/' . $image->thumb_path }}">
+                <div class="relative pb-[calc(33.3%-10px)] w-[calc(33.3%-10px)] overflow-hidden ripple" data-bs-toggle="modal"
+                    data-bs-target="#imageViewer" data-img-src="{{ '/anhvavideo\/' . $image->path }}">
+                    <img class="absolute h-full w-full object-cover" src="{{ '/anhvavideo\/' . $image->thumb_path }}">
                 </div>
             @endforeach
         </div>
@@ -19,36 +19,36 @@
                     YouTube</span>
                 <span style="align-self:center;height: 1px;flex:1;background-color:#C0BFBC"></span>
             </div>
-            <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px">
+            <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px;margin-right: -10px">
                 @php
                     $video_youtube = array_filter($videos, function ($value) {
                         return $value->type == 'youtube';
                     });
                 @endphp
                 @foreach ($video_youtube as $video)
-                    <div class="w-[32.1%] h-auto ripple" data-bs-toggle="modal" data-bs-target="#youtubeVideoViewer"
-                        data-video-id="{{ trim($video->link_youtube, 'https://www.youtube.com/watch?v=') }}">
-                        <img style="height: 180px;width: 100%;object-fit:cover"
-                            src="{{ '/anhvavideo/' . $video->thumb_path }}">
+                    <div class="relative pb-[calc(33.3%-10px)] w-[calc(33.3%-10px)] overflow-hidden ripple"
+                        data-bs-toggle="modal" data-bs-target="#youtubeVideoViewer"
+                        data-video-id="{{ $video->link_youtube }}">
+                        <img class="absolute h-full w-full object-cover" src="{{ '/anhvavideo/' . $video->thumb_path }}">
                     </div>
                 @endforeach
             </div>
-            <div style="display: flex;flex-direction:row;">
+            <div style="display: flex;flex-direction:row;margin-top:10px">
                 <span style="font-weight:700;font-size:20px;color: #C0BFBC;padding-right: 15px;line-height: 50px">Video đã
                     tải lên</span>
                 <span style="align-self:center;height: 1px;flex:1;background-color:#C0BFBC"></span>
             </div>
-            <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px">
+            <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px;margin-right: -10px">
                 @php
                     $video_html5 = array_filter($videos, function ($value) {
                         return $value->type == 'html5';
                     });
                 @endphp
                 @foreach ($video_html5 as $video)
-                    <div class="w-[32.1%] h-auto ripple" data-bs-toggle="modal" data-bs-target="#videoViewer"
+                    <div class="relative pb-[calc(33.3%-10px)] w-[calc(33.3%-10px)] overflow-hidden ripple"
+                        data-bs-toggle="modal" data-bs-target="#videoViewer"
                         data-video-src="{{ '/anhvavideo/' . $video->path }}">
-                        <img style="height: 180px;width: 100%;object-fit:cover"
-                            src="{{ '/anhvavideo/' . $video->thumb_path }}">
+                        <img class="absolute h-full w-full object-cover" src="{{ '/anhvavideo/' . $video->thumb_path }}">
                     </div>
                 @endforeach
             </div>
@@ -59,7 +59,7 @@
 
 @section('modal')
     <div class="modal fade" id="imageViewer" tabindex="-1" aria-labelledby="imageViewerLabel" aria-hidden="true">
-        <div class="modal-dialog" style="width: 600px;max-width: none;">
+        <div class="modal-dialog" style="width: 100%;max-width: 600px;">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="imageViewerLabel">Xem ảnh</h5>
@@ -115,7 +115,7 @@
         });
     </script>
     <div class="modal fade" id="videoViewer" tabindex="-1" aria-labelledby="videoViewerLabel" aria-hidden="true">
-        <div class="modal-dialog" style="width: 600px;max-width: none;">
+        <div class="modal-dialog" style="width: 100%;max-width: 600px;">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="videoViewerLabel">Xem video</h5>
@@ -184,7 +184,7 @@
     <!-- YouTube Video Viewer Modal -->
     <div class="modal fade" id="youtubeVideoViewer" tabindex="-1" aria-labelledby="youtubeVideoViewerLabel"
         aria-hidden="true">
-        <div class="modal-dialog modal-lg">
+        <div class="modal-dialog" style="width: 100%;max-width: 600px;">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="videoViewerLabel">YouTube Video</h5>
@@ -193,22 +193,62 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <iframe id="youtubeVideoIframe" width="100%" height="500" src="" frameborder="0"
+                    <iframe id="youtubeVideoIframe" width="100%" height="360" src="" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen></iframe>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary prevVideo" id="prevVideo">{{ '<' }}</button>
+                    <button type="button" class="btn btn-primary nextVideo" id="nextVideo">></button>
                 </div>
             </div>
         </div>
     </div>
     <script>
         $(document).ready(function() {
-            $('.ripple').click(function() {
-                var videoId = $(this).data('video-id');
-                var iframeUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+            var videoIds = $('.ripple').map(function() {
+                return $(this).data('video-id'); // Assuming this is the YouTube video ID or URL
+            }).get();
+            var currentVideoIndex = 0;
+
+            // Function to update the video in the modal
+            function updateVideo(index) {
+                var videoId = videoIds[index];
+                // If videoId is a URL, extract the ID
+                videoId = videoId.includes('youtu') ? youtube_parser(videoId) : videoId;
+                var iframeUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`;
                 $('#youtubeVideoIframe').attr('src', iframeUrl);
+            }
+
+            // Function to parse YouTube video ID from URL
+            function youtube_parser(url) {
+                var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+                var match = url.match(regExp);
+                return (match && match[7].length == 11) ? match[7] : false;
+            }
+
+            // Click event for opening the modal and setting the first video
+            $('.ripple').click(function() {
+                currentVideoIndex = $('.ripple').index(this);
+                updateVideo(currentVideoIndex);
             });
 
-            $('#videoViewer').on('hide.bs.modal', function() {
+            // Next button functionality
+            $('.nextVideo').click(function() {
+                currentVideoIndex = (currentVideoIndex + 1) % videoIds
+                    .length; // Loop back to the first video
+                updateVideo(currentVideoIndex);
+            });
+
+            // Previous button functionality
+            $('.prevVideo').click(function() {
+                currentVideoIndex = (currentVideoIndex - 1 + videoIds.length) % videoIds
+                    .length; // Loop back to the last video
+                updateVideo(currentVideoIndex);
+            });
+
+            // Clear the video when the modal is closed
+            $('#youtubeVideoViewer').on('hide.bs.modal', function() {
                 $('#youtubeVideoIframe').attr('src', '');
             });
         });
--- app/views/pages/gallery/index.blade.php
+++ app/views/pages/gallery/index.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Thư viện ảnh')
 @section('background-color', '#ededed')
 @section('content')
-    <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px">
+    <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px;margin-right: -10px">
         @foreach ($albums as $album)
-            <div class="w-[49%] h-auto p-[10px] bg-white rounded-xl ripple shadow-md"
+            <div class="w-[calc(50%-10px)] h-auto p-[10px] bg-white rounded-xl ripple shadow-md"
                 goto="/gallery/album/{{ $album->id }}">
                 <img style="height: 180px;width: 100%;border-radius: 10px;object-fit:cover"
                     src="{{ '/anhvavideo\/' . $album->bg_image }}">
