a7d320d9fa76af9c298df492d6e2ab94597c9ed1
Duong Tung Anh <tunnaduong@gmail.com>
2024-06-29 15:52:39 +0700

    refactor: Update gallery page layout and fix image display issue

--- app/views/pages/gallery/index.blade.php
+++ app/views/pages/gallery/index.blade.php
@@ -5,10 +5,12 @@
 @section('content')
     <div style="display: flex;flex-direction: row;flex-wrap: wrap;gap: 10px;margin-right: -10px">
         @foreach ($albums as $album)
-            <div class="w-[calc(50%-10px)] h-auto p-[10px] bg-white rounded-xl ripple shadow-md"
+            <div class="w-[calc(50%-10px)] p-[10px] bg-white rounded-xl ripple shadow-md"
                 goto="/gallery/album/{{ $album->id }}">
-                <img style="height: 180px;width: 100%;border-radius: 10px;object-fit:cover"
-                    src="{{ '/anhvavideo\/' . $album->bg_image }}">
+                <div class="relative pb-[70%]">
+                    <img class="absolute h-full w-full object-cover" style="border-radius: 10px;"
+                        src="{{ '/anhvavideo\/' . $album->bg_image }}">
+                </div>
                 <h2 style="font-weight: 600;font-size:19px;margin-top:10px">{{ $album->name }}</h2>
                 <p>{{ $album->total_pic }} {{ $album->type }}</p>
             </div>
