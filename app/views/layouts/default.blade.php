2b602cf328b8d23b83d3c52088dbd2028b1aa8ce
Dương Tùng Anh <tunnaduong@gmail.com>
2024-07-24 13:48:54 +0700

    Update default.blade.php

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -2,6 +2,15 @@
 <html>
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YZG381JXWG"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-YZG381JXWG');
+    </script>
     <title>@yield('title') - C4K60</title>
     @include('includes.head')
 </head>
