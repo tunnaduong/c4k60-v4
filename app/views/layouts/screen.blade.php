7b50795f1c22f88e0698dae072b3ac52ae71e299
Dương Tùng Anh <tunnaduong@gmail.com>
2024-07-24 13:56:02 +0700

    Update screen.blade.php

--- app/views/layouts/screen.blade.php
+++ app/views/layouts/screen.blade.php
@@ -11,6 +11,8 @@
     
       gtag('config', 'G-YZG381JXWG');
     </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3425905751761094"
+     crossorigin="anonymous"></script>
     <title>@yield('title') - C4K60</title>
     @include('includes.head')
 </head>
