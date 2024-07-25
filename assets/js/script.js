da54438f34254ef91d954f6ead6bada0da1027f2
Duong Tung Anh <tunnaduong@gmail.com>
2024-07-25 10:49:55 +0700

    fixed compliments bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,40 @@
 $body = $("#screen");
+
+function loadAll() {
+  const subjects = document.querySelectorAll(".texttruncate");
+
+  subjects.forEach((subject) => {
+    let text = subject.textContent;
+    if (text.length > 87) {
+      subject.textContent = `${text.slice(0, 90)}...`;
+    }
+  });
+
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+
+  if ((hours >= 5 && hours < 10) || (hours === 10 && minutes < 30)) {
+    $("#loiChuc").html("Chúc bạn có một ngày mới năng động và hiệu quả.");
+  } else if ((hours >= 11 && hours < 13) || (hours == 10 && minutes >= 30)) {
+    $("#loiChuc").html(
+      "Chúc bạn có một buổi trưa thật vui vẻ, ngập tràn năng lượng."
+    );
+  } else if ((hours >= 13 && hours < 18) || (hours === 18 && minutes < 30)) {
+    $("#loiChuc").html(
+      "Chúc bạn có một buổi chiều vui vẻ và cả ngày hạnh phúc!"
+    );
+  } else if ((hours >= 18 && hours < 22) || (hours === 22 && minutes < 30)) {
+    $("#loiChuc").html(
+      "Chúc cậu có một buổi tối an lành, vui vẻ nhé. Good night!"
+    );
+  } else {
+    $("#loiChuc").html(
+      "Nằm xuống giường đi và mơ những giấc mơ ngọt ngào nhất bạn nhé!"
+    );
+  }
+}
+
 function initWaves() {
   Waves.attach(".user-greeting");
   Waves.attach(".menu--search");
@@ -44,41 +80,6 @@ $(document).ajaxComplete(function () {
 
 function go(url) {
   window.location.href = url;
-}
-
-function loadAll() {
-  const subjects = document.querySelectorAll(".texttruncate");
-
-  subjects.forEach((subject) => {
-    let text = subject.textContent;
-    if (text.length > 87) {
-      subject.textContent = `${text.slice(0, 90)}...`;
-    }
-  });
-
-  var today = new Date();
-  var hours = today.getHours();
-  var minutes = today.getMinutes();
-
-  if ((hours >= 5 && hours < 10) || (hours === 10 && minutes < 30)) {
-    $("#loiChuc").html("Chúc bạn có một ngày mới năng động và hiệu quả.");
-  } else if (hours >= 11 && hours < 13) {
-    $("#loiChuc").html(
-      "Chúc bạn có một buổi trưa thật vui vẻ, ngập tràn năng lượng."
-    );
-  } else if ((hours >= 13 && hours < 18) || (hours === 18 && minutes < 30)) {
-    $("#loiChuc").html(
-      "Chúc bạn có một buổi chiều vui vẻ và cả ngày hạnh phúc!"
-    );
-  } else if ((hours >= 18 && hours < 22) || (hours === 22 && minutes < 30)) {
-    $("#loiChuc").html(
-      "Chúc cậu có một buổi tối an lành, vui vẻ nhé. Good night!"
-    );
-  } else {
-    $("#loiChuc").html(
-      "Nằm xuống giường đi và mơ những giấc mơ ngọt ngào nhất bạn nhé!"
-    );
-  }
 }
 
 $("html").on("click", "[href]", function (e) {
